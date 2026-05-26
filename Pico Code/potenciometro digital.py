058a2dfeab457f6bf3565d609033a7b0479d3dc3
Bernardo Pironio <bpironio@gmail.com>
2026-05-26 10:40:41 -0300

    Agrego codigo inicial potenciometro

---
+++ Pico Code/potenciometro digital.py
@@ -0,0 +1,20 @@
+from machine import Pin, ADC
+import time
+
+cs = Pin(15,Pin.OUT)
+inc = Pin(14,Pin.OUT)
+ud = Pin(13,Pin.OUT)
+
+
+cs.value(0)
+inc.value(1)
+ud.value(0)
+
+
+while True:
+    inc.value(0)
+    time.sleep(.1)
+    inc.value(1)
+    time.sleep(0.5)
+
+
